c11a9f52c717405cc75d98c6bfb8d7c9b46ed9c4
xdx97 <1140459171@qq.com>
2022-10-28 20:28:56 +0800

    'otate'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-view/>   <!-- 占位  -->
+    <router-view/><!-- 占位  -->   
     <main-tab-bar/>
   </div>
 </template>
--- src/components/content/MainTabBar.vue
+++ src/components/content/MainTabBar.vue
@@ -1,26 +1,36 @@
 <template>
   <tab-bar>
-    <tab-bar-item  path="/home" activeColor="rgb(0, 115, 255)">
-      <template #item-icon><img src="~assets/img/tabbar/home.png" ></template>
-      <template #item-icon-active><img src="~assets/img/tabbar/home_active.png" ></template>
+    <tab-bar-item path="/home" activeColor="rgb(0, 115, 255)">
+      <template #item-icon><img src="~assets/img/tabbar/home.png" /></template>
+      <template #item-icon-active
+        ><img src="~assets/img/tabbar/home_active.png"
+      /></template>
       <template #item-text><div>首页</div></template>
     </tab-bar-item>
-    <tab-bar-item  path="/about" activeColor="rgb(0, 115, 255)">
-      <template #item-icon><img name="item-icon" src="~assets/img/tabbar/shopping.png" ></template>
-      <template #item-icon-active><img src="~assets/img/tabbar/shopping_active.png" ></template>
+    <tab-bar-item path="/about" activeColor="rgb(0, 115, 255)">
+      <template #item-icon
+        ><img name="item-icon" src="~assets/img/tabbar/shopping.png"
+      /></template>
+      <template #item-icon-active
+        ><img src="~assets/img/tabbar/shopping_active.png"
+      /></template>
       <template #item-text><div name="item-text">订单</div></template>
     </tab-bar-item>
-    <tab-bar-item  path="/profile" activeColor="rgb(0, 115, 255)">
-      <template #item-icon><img name="item-icon" src="~assets/img/tabbar/profile.png" ></template>
-      <template #item-icon-active><img src="~assets/img/tabbar/profile_active.png" ></template>
+    <tab-bar-item path="/profile" activeColor="rgb(0, 115, 255)">
+      <template #item-icon
+        ><img name="item-icon" src="~assets/img/tabbar/profile.png"
+      /></template>
+      <template #item-icon-active
+        ><img src="~assets/img/tabbar/profile_active.png"
+      /></template>
       <template #item-text><div name="item-text">我的</div></template>
     </tab-bar-item>
   </tab-bar>
 </template>
 
 <script>
-import TabBar from 'components/common/TabBar.vue'
-import TabBarItem from 'components/common/TabBarItem.vue'
+import TabBar from "components/common/TabBar.vue";
+import TabBarItem from "components/common/TabBarItem.vue";
 
 export default {
   name: "MainTabBar",
